2ca0deb9c3dc4f390bfb3dc72e95fbbe4455f13d
Braimah101 <A>
2021-11-22 13:30:12 +0000

    add framework

---
+++ src/main/java/co/uk/zoopla7Test/App.java
@@ -0,0 +1,13 @@
+package co.uk.zoopla7Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
